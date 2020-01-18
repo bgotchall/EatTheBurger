9475fbbc70f64861757f2a3b7bdf7eddc29c4afe
austinev <bgotchall@gmail.com>
2020-01-17 20:13:00 -0600

    some basic ORM functionality

---
+++ config/orm.js
@@ -0,0 +1,25 @@
+var connection=require("connection.js");
+
+// selectAll()
+// insertOne()
+// updateOne()
+
+selectAll()
+insertOne()
+updateOne()
+
+var orm={
+selectAll: function(cb){
+let queryString="SELECT * FROM burgers";
+connection.query(queryString,function(req,res){
+    if (err) {throw err;}
+    else cb(res);
+});
+
+
+},
+otherthing: function(){
+
+},
+
+};
